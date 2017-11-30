439a59746530fdc4b616de2843b87dc3d3bdbc81
Pablo Winant <pablo.winant@gmail.com>
2017-11-30 15:08:20 +0100

    Update challenge_1

--- challenge_1.jl
+++ challenge_1.jl
@@ -1,6 +1,7 @@
+# to launch notebook from atom:
+# - load julia kernel: `using IJulia`
+# - run notebook: `notebook()`
 
-
-a = 6
 
 function f(a::Int)
     z = a+3
@@ -30,21 +31,104 @@ end
 #    - simulate a VAR1 process for T periods N times
 #    - compare theoretical findings for ergodic distribution and simulations based ones
 
-# 2/ discretize VAR1 process
-#    - try first with a 1-d process using tauchen or Rouwenhorst method from Quantecon
-#    - discretize a VAR process when the covariance matrix is diagonal
+include("ar1.jl")
 
-# 3/ create functions (keep the same name for all functions):
-#    - to simulate an VAR1 specified by three matrices
-#    - to simulate an VAR1 specified by two matrices, assuming mean is zero
-#    - to simulate an VAR1 many times
-#      (do and replace simulate by "compute ergodic distribution" below)
+ρ = 0.9
+A = eye(2)*ρ
+σ = 0.001
+Σ=eye(2)*σ^2
 
-# 4/ Refactoring:
-#    - Create a new file with a Processes module to host the functions.
-#    - Write another test files which imports from the module and shows how to use the functions.
-#    - Devise various tests that actually check the functions are correct. (use @assert macro)
 
-# 5/ Create a VAR1 type (in the module). Add new functions that operate on this type. Add them to the test file.
+Simulation.simulate(A,Σ)
 
-# 6/ Bonus: compute likelyhood function
+@time Simulation.simulate(A,Σ,X_0,10000)
+
+
+X_0 = zeros(2)
+
+using StaticArrays
+
+s_X_0 = SVector(X_0...)
+s_A = SMatrix{2,2}(A)
+s_Σ = SMatrix{2,2}(Σ)
+
+
+@time Simulation.simulate(s_A,s_Σ,s_X_0, 10000)
+
+
+Z = zeros(2,10)
+
+s_Z = reinterpret(SVector{2,Float64}, Z, (10,))
+s_Z[2]
+
+
+σ_e = σ/sqrt(1-ρ^2)
+
+S, nit = Simulation.ergodic_distribution(A,Σ)
+sqrt(S[1,1])
+
+@assert abs(sqrt(S[1,1])-σ_e)<1e-6
+
+
+
+# unpacking
+
+obj = linspace(0,1,1000)
+typeof(obj)
+[x for x in obj]
+collect(obj)
+[obj...]
+
+
+function special_sum(x1, x2, x3)
+    return 1*x1 + 2*x2 + 3*x3
+end
+
+special_sum(1,2,3)
+
+
+function special_sum_n(l::Vector{Float64})
+    n = length(l)
+    if n>1
+        return special_sum_n(l[1:n-1])+n*l[n]
+    else
+        return l[1]
+    end
+end
+
+special_sum_n([1,23,4.0])
+
+
+function special_sum_n(l::Vector{Float64})
+    n = length(l)
+    s = 0.0
+    for i=1:n
+        s+=l[i]*i
+    end
+    return s
+end
+
+function special_sum_g(l...)
+    n = length(l)
+    s = 0.0
+    for i=1:n
+        s+=l[i]*i
+    end
+    return s
+end
+
+special_sum_n([1,23,4.0])
+
+
+special_sum_g(1,23,4.0,10,10)
+
+
+special_sum_g(linspace(0,1,1000000)...)
+
+
+v = rand(2)
+x1 = v[1]
+x2 = v[2]
+sv = SVector{2}(x1,x2)
+
+sv = SVector{2}(v...)
